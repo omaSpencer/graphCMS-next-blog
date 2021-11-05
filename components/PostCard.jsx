0eac877568d108b1e3615bf7b3c9d44a23fa6e0f
Zoltan Busi <oma.spencer@gmail.com>
2021-11-05 21:30:40 +0100

    edit readme

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -7,7 +7,7 @@ const PostCard = ({ post }) => {
 	return (
 		<div className="bg-white shadow-lg rounded-lg p-0 lg:p-8 pb-12 mb-8">
 			<div className="relative overflow-hidden shadow-md pb-80 mb-6">
-				<Image src={post.featuredImage.url} layout="fill" priority />
+				<Image src={post?.featuredImage?.url} layout="fill" priority />
 			</div>
 		</div>
 	);
